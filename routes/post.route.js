81ba0a94be060ef65077fe7b6f62f07ed8d81e9c
Nabonb <nabobaruanb@gmail.com>
2024-11-28 10:08:36 +0600

    debugging still

--- routes/post.route.js
+++ routes/post.route.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/test", (req, res) => {
-  res.send("this is working!");
+  res.send("this!");
   console.log("router work");
 });
 
