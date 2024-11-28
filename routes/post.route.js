273394308894325c4f7e07786b55400bb7d689c3
Nabonb <nabobaruanb@gmail.com>
2024-11-28 12:45:12 +0600

    hash password installed and done

--- routes/post.route.js
+++ routes/post.route.js
@@ -2,9 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("this!");
-  console.log("router work");
-});
+// router.get("/test", (req, res) => {
+//   console.log("router work");
+// });
+// router.post("/test", (req, res) => {
+//   console.log("router work");
+// });
+// router.put("/test", (req, res) => {
+//   console.log("router work");
+// });
+// router.delete("/test", (req, res) => {
+//   console.log("router work");
+// });
 
 export default router;
