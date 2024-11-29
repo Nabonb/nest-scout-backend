84cf32418fc0a3a90b3265158ac0aa9023443e27
Nabonb <nabobaruanb@gmail.com>
2024-11-30 00:00:39 +0600

    cookie parser installed and setup successfully

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json()); // to parse json request
+app.use(cookieParser()); // to parse cookie request
+
+// app.use("/", (req, res) => {
+//   res.send("Server is running");
+// });
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -17,17 +17,49 @@ export const register = async (req, res) => {
         password: hashedPassword,
       },
     });
-
-    console.log(newUser);
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
     console.log(err.message);
     res.status(500).json({ message: "Failed to create user" });
   }
 };
-export const login = (req, res) => {
-  //db operations
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //CHECK IF USER EXISTS
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid Creadentials" });
+
+    //CHECK IF THE PASSWORD IS CORRECT
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Creadentials" });
+
+    //GENERATE COOKIE TOKEN AND SEND IT TO THE USER
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("Success");
+
+    const age = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    res
+      .cookie("test2", "myValue", {
+        httpOnly: true,
+        //secure: true, //for http request for deployment
+        maxAge: age, // it expires after 7 days
+      })
+      .status(200)
+      .json({ message: "Login successful" });
+  } catch (error) {
+    console.log(err.message);
+    res.status(500).json({ message: "Failed to login" });
+  }
 };
+
 export const logout = (req, res) => {
   //db operations
 };
