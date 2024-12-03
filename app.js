673ea545d0646dfd6fe7020f6cd250fa169fa46c
Nabonb <nabobaruanb@gmail.com>
2024-12-03 12:37:48 +0600

    authentication and authorization as admin done with token in cookie

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(cookieParser()); // to parse cookie request
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(5000, () => {
   console.log("server is running");
